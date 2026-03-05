627e88261424fb3effb326d520510308865e944d
Mason Howard <mshoward42@gmail.com>
2026-03-04 19:47:33 -0600

    default dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { ComponentExample } from "@/components/component-example";
+import {ThemeProvider } from "@/components/theme-provider"
 
 export function App() {
-return <ComponentExample />;
+return 
+(
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ComponentExample />
+    </ThemeProvider>
+);
 }
 
 export default App;
