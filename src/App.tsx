354fbd8163f9725dab49c4c89f0c7287694c491f
Mason Howard <mshoward42@gmail.com>
2026-03-05 09:29:56 -0600

    sidebar updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ComponentExample } from "@/components/component-example";
+import Layout from "@/components/layout";
+//import { Layout } from "@/components/layout";
 import {ThemeProvider } from "@/components/theme-provider"
 
 export function App() {
 return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <ComponentExample />
+        <Layout>
+            <ComponentExample />
+        </Layout>
     </ThemeProvider>
     );
 }
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,17 +1,50 @@
+import * as React from "react"
+import { Button } from "@/components/ui/button"
+import {ChevronDown, ChevronsUpDown, Pen, Plus} from "lucide-react"
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
+
 
 export function AppSidebar() {
+  const [isOpen, setIsOpen] = React.useState(false)
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        Character Manager
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
+        <Collapsible className="group/collapsible">
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                Design
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href="#">Character Sheet <Plus className="m-auto"/> </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter />
