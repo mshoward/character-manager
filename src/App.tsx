c4b5af52fd8eb34065b5c4ecf3dffd0bbae7202e
Mason Howard <mshoward42@gmail.com>
2026-03-04 19:57:38 -0600

    add all shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import { ComponentExample } from "@/components/component-example";
 import {ThemeProvider } from "@/components/theme-provider"
 
 export function App() {
-return 
-(
+return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <ComponentExample />
     </ThemeProvider>
-);
+    );
 }
 
 export default App;
